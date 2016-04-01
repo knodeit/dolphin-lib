82ad200958376adf7765796bdfb61d1d6f2b641b
Vadim <vadim@lamppostlabs.com>
2016-04-01 13:17:41 -0400

    fixed required rule

--- validator/classes/rules/requiredRule.js
+++ validator/classes/rules/requiredRule.js
@@ -10,12 +10,23 @@ var validator = require('validator');
 
 exports.validate = function (field, fieldParams, params, value) {
     var message = 'The field is required';
+    try {
+        if (validator.isBoolean(value) || validator.isNumeric(value)) {
+            return Q.resolve();
+        }
 
-    if (validator.isBoolean(value) || validator.isNumeric(value)) {
-        return Q.resolve();
-    }
+        if (value === undefined || value === null) {
+            throw new Error();
+        }
 
-    if ((value instanceof Array && value.length === 0) || (value === undefined || value == null || value.trim().length === 0)) {
+        if (value instanceof Array && value.length === 0) {
+            throw new Error();
+        }
+
+        if (value instanceof String && value.trim().length === 0) {
+            throw new Error();
+        }
+    } catch (e) {
         return Q.reject(new KNRuleException(field, KNErrorCodes.required, fieldParams && fieldParams.message ? fieldParams.message : message));
     }
 
