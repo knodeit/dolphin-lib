8d7fd632401ac5220f6c11ad8bfbf3e0c4ca7668
Vadim <vadim@lamppostlabs.com>
2016-07-27 13:33:20 -0400

    fixed sms

--- twilio/index.js
+++ twilio/index.js
@@ -6,25 +6,22 @@
 var Q = require('q');
 
 
-function Twilio(accountSID,authToken,phoneNumber) {
-    this.service = require('twilio');
-    this.service.accountSID = accountSID;
-    this.service.authToken = authToken;
-    this.service.phoneNumber = phoneNumber;
+function Twilio(accountSID, authToken, phoneNumber) {
+    this.phoneNumber = phoneNumber;
+    this.client = require('twilio')(accountSID, authToken);
 }
 
 
-Twilio.prototype.sendSMS = function (toNumber,text) {
+Twilio.prototype.sendSMS = function (toNumber, text) {
     var deferred = Q.defer();
     var message = {
         to: '+' + toNumber,
-        from: this.service.phoneNumber,
+        from: this.phoneNumber,
         body: text
     };
 
-    var client = new this.service.RestClient(this.service.accountSID, this.service.authToken);
-    client.sms.messages.create(message,function(err,result){
-        if (err){
+    this.client.sendMessage(message, function (err, result) {
+        if (err) {
             return deferred.reject(err);
         }
         return deferred.resolve(result);
